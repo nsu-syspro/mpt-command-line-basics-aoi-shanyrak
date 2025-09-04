2fc80dde0d913ea05102345999899641c94992f1
aoi-shanyrak <maxim.mezheniy@gmail.com>
2025-09-04 17:55:40 +0700

    update

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 tn1=$(mktemp -d)
-cd ~/mpt-command-line-basics-aoi-shanyrak
 echo $(unzip -p data/archive-part2.zip) > $tn1/arch1.txt
 tar -xf data/archive-part1.tar
 echo $(cat some/directory/baz.txt) > $tn1/arch2.txt
