1d26dcb5c75ec73dc0d042d00eaec3dd30e7cbd6
aoi-shanyrak <maxim.mezheniy@gmail.com>
2025-09-05 18:15:42 +0700

    update 2.0

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 tn1=$(mktemp -d)
-echo $(unzip -p data/archive-part2.zip) > $tn1/arch1.txt
+unzip -q data/archive-part2.zip -d $tn1
 tar -xf data/archive-part1.tar
-echo $(cat some/directory/baz.txt) > $tn1/arch2.txt
-tar -czvf data/archive-combined.tar.gz -C $tn1 arch1.txt arch2.txt
+cp -r some/* $tn1/
+tar -czf data/archive-combined.tar.gz -C $tn1 .
+rm -rf $tn1 some
 
 
 
