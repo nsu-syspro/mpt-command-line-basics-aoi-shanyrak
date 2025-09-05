da3465ead719be529f73ad93eb8cb267412e59c5
aoi-shanyrak <maxim.mezheniy@gmail.com>
2025-09-05 18:22:45 +0700

    update 2.1

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 tn1=$(mktemp -d)
 unzip -q data/archive-part2.zip -d $tn1
-tar -xf data/archive-part1.tar
-cp -r some/* $tn1/
+tar -xf data/archive-part1.tar -C $tn1
 tar -czf data/archive-combined.tar.gz -C $tn1 .
 rm -rf $tn1 some
 
